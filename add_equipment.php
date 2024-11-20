4a8a206876f51efd80f727bf00b9f1c11cd97e75
jah09 <jasuhot1@gmail.com>
2024-11-20 22:50:25 +0800

    bugfix/delete-members

--- add_equipment.php
+++ add_equipment.php
@@ -23,6 +23,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($conn->query($insertQuery) === TRUE) {
         $response['success'] = true;
+        $response['message'] = 'Equipment added successfully! 
+         ';
     }
 }
 ?>
